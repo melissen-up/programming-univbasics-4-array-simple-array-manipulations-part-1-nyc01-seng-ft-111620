2a22907a443678ecf9ade5e8e05c0361a8c052d2
Melissa F <melissa.faulner@gmail.com>
2020-11-01 19:07:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#def using_pop(array)
+#end
+
+#def pop_with_args
+#end
+
+#def using_shift
+#end
+
+#def shift_with_args
+#end
